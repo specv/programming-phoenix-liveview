e0a1852bdd277aaa9722bd21f7e76ae62e0be2ea
zhoukan <zhoukan@mozigu.cn>
2023-01-24 00:45:28 +0800

    Ch1: finish the game

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,13 @@ defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(
+      socket,
+      score: 0,
+      message: "Make a guess:",
+      finished?: false,
+      target: Enum.random(1..10)
+    )}
   end
 
   def render(assigns) do
@@ -11,20 +17,35 @@ defmodule PentoWeb.WrongLive do
     <h2>
       <%= @message %>
     </h2>
-    <h2>
-      <%= for n <- 1..10 do %>
-        <a href="#" phx-click="guess" phx-value-number={n}> <%= n %> </a>
-      <% end %>
-    </h2>
+    <%= if @finished? do %>
+      <a href="#" phx-click="retry"> Retry </a>
+    <% else %>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <a href="#" phx-click="guess" phx-value-number={n}> <%= n %> </a>
+        <% end %>
+      </h2>
+    <% end %>
     """
   end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again. "
+    {finished?, message} =
+      if String.to_integer(guess) == socket.assigns.target do
+        {true, "Your guess: #{guess}. You Win!"}
+      else
+        {false, "Your guess: #{guess}. Wrong. Guess again."}
+      end
     score = socket.assigns.score - 1
+
     {
       :noreply,
-      assign(socket, message: message, score: score)
+      assign(socket, message: message, score: score, finished?: finished?)
     }
+  end
+
+  def handle_event("retry", _, socket) do
+    {:ok, socket} = mount(%{}, %{}, socket)
+    {:noreply, socket}
   end
 end
